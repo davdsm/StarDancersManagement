61681f871ac00213f4f81eecf68f3fafd2d7ce33
davdsm <geral@davdsm.pt>
2022-11-30 17:00:15 +0000

    fix update contact

--- StarDancers/src/components/table/ModalTable.vue
+++ StarDancers/src/components/table/ModalTable.vue
@@ -133,7 +133,7 @@ export default {
                 >Contacto Responsável</label
               >
               <input
-                type="number"
+                type="text"
                 name="phone-number"
                 id="phone-number"
                 class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
